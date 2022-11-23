3345ab8cea133364ee623c8c83b2cded064a9d9b
Hyun Woo <hwsa10041@gmail.com>
2022-11-23 18:46:34 +0900

    test

--- src/main/java/project/BBolCha/domain/user/Service/UserService.java
+++ src/main/java/project/BBolCha/domain/user/Service/UserService.java
@@ -172,7 +172,7 @@ public class UserService {
     }
 
     public ResponseEntity<String> version() {
-        String version = "version 0.0.1";
+        String version = "version 0.0.2";
         return new ResponseEntity<>(version,HttpStatus.OK);
     }
 }
